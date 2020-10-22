8520d6b133b8f28eab9aa7c06d8ddb2577d1d449
arjuntvn <14861035+arjuntvn@users.noreply.github.com>
2020-10-22 22:43:05 +0530

    String rev brute force

---
+++ StringRev.java
@@ -0,0 +1,12 @@
+package practise;
+
+public class StringRev {
+
+	public static void main(String[] args) {
+	
+		int inputArray[]= {1,3,2};
+		for(int i=inputArray.length;i>0;i--) {
+			System.out.println(inputArray[i-1]);
+		}
+	}
+}
